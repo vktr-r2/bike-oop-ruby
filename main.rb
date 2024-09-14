f260415950a91ec0ced87d20f8d055069875c0c2
Viktor R <122321007+vktr-r2@users.noreply.github.com>
2024-09-13 21:32:28 -0400

    CH2: Add rim and tire instance variables

--- main.rb
+++ main.rb
@@ -1,15 +1,23 @@
 class Gear
-    attr_reader :chainring, :cog
+    attr_reader :chainring, :cog, :rim, :tire
     
-    def initialize(chainring, cog)
+    def initialize(chainring, cog, rim, tire)
         @chainring = chainring
         @cog = cog
+        @rim = rim
+        @tire = tire
     end
 
     def ratio
         chainring / cog.to_f
     end
+
+    def gear_inches
+        ratio * (rim + (tire * 2))
+    end
 end
 
-puts Gear.new(52, 11).ratio # 4.7272727272727275
-puts Gear.new(30, 27).ratio # 1.1111111111111112
+puts Gear.new(52, 11, 26, 1.5).ratio # 4.7272727272727275
+puts Gear.new(30, 27, 24, 1.25).ratio # 1.1111111111111112
+puts Gear.new(52, 11, 26, 1.5).gear_inches # 137.0909090909091
+puts Gear.new(30, 27, 24, 1.25).gear_inches # 125.27272727272728
