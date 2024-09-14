92b370c44cdddc25940eb95234938c8278ee97bb
Viktor R <122321007+vktr-r2@users.noreply.github.com>
2024-09-13 22:46:36 -0400

    Add Wheel class and refactor Gear

--- main.rb
+++ main.rb
@@ -1,11 +1,10 @@
 class Gear
-    attr_reader :chainring, :cog, :rim, :tire
+    attr_reader :chainring, :cog, :wheel
     
-    def initialize(chainring, cog, rim, tire)
+    def initialize(chainring, cog, wheel=nil)
         @chainring = chainring
         @cog = cog
-        @rim = rim
-        @tire = tire
+        @wheel = wheel
     end
 
     def ratio
@@ -13,11 +12,39 @@ class Gear
     end
 
     def gear_inches
-        ratio * (rim + (tire * 2))
+        ratio * wheel.diameter
     end
 end
 
+class Wheel
+    
+    attr_reader :rim, :tire
+
+    def initialize(rim, tire)
+        @rim = rim
+        @tire = tire
+    end
+
+    def diameter
+        rim + (tire * 2)
+    end
+
+    def circumference
+        diameter * Math::PI 
+    end
+end
+
+=begin
 puts Gear.new(52, 11, 26, 1.5).ratio # 4.7272727272727275
 puts Gear.new(30, 27, 24, 1.25).ratio # 1.1111111111111112
 puts Gear.new(52, 11, 26, 1.5).gear_inches # 137.0909090909091
 puts Gear.new(30, 27, 24, 1.25).gear_inches # 125.27272727272728
+=end
+
+@wheel = Wheel.new(26, 1.5)
+puts @wheel.circumference
+
+puts Gear.new(52, 11, @wheel).gear_inches
+
+puts Gear.new(52, 11).ratio
+
