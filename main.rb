ddc24f1baf762add7051c9cf60b1bc9d48f860fc
Viktor R <122321007+vktr-r2@users.noreply.github.com>
2024-09-29 22:37:02 -0400

    CH-6 Add Bicycle class

--- main.rb
+++ main.rb
@@ -50,4 +50,42 @@ puts wheel.circumference
 puts Gear.new(52, 11, wheel).gear_inches
 
 puts Gear.new(52, 11).ratio
+puts Gear.new(52, 11).ratio
 
+
+
+# CHAPTER 6: Aquiring Behaviour Through Inheritance
+
+class Bicycle
+    attr_reader :size, :chain, :tire_size
+
+    def initialize(args={})
+        @size           = args[:size]
+        @chain          = args[:chain]      || default_chain
+        @tire_size      = args[:tire_size]  || default_tire_size
+        post_initialize(args)
+
+    end
+
+    def spares
+        { tire_size: tire_size,
+        chain: chain}.merge(local_spares)
+    end
+
+    def default_tire_size
+        raise NotImplementedError
+    end
+
+    def post_initialize(args)
+        nil
+    end
+
+    def local_spares
+        {}
+    end
+
+    def default_chain
+        "10-speed"
+    end
+
+end
