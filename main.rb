b2bfdecc83562f80c35006a619c7d63c16c6a318
Viktor R <122321007+vktr-r2@users.noreply.github.com>
2024-09-29 22:41:55 -0400

    CH-6 Add MountainBike subclass

--- main.rb
+++ main.rb
@@ -105,3 +105,20 @@ class RoadBike < Bicycle
         "23"
     end
 end
+
+class MountainBike < Bicycle
+    attr_reader :front_shock, :rear_shock
+
+    def post_initialize(args)
+        @front_shock = args[:front_shock]
+        @rear_shock = args[:rear_shock]
+    end
+
+    def local_spares
+        {read_shock: rear_shock}
+    end
+
+    def default_tire_size
+        "2.1"
+    end
+end
